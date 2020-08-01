68dda2a015a75e9bab3a2626b5db65e8de8741f6
bchavdarov <spo@abv.bg>
2020-08-01 12:08:25 +0300

    added some style

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7efe0f0809572c40ddf574bb8d4d1dbb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '83537420f3ebcad5cbd7e1b3be9fec32');
