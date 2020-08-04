850b44acda4d4c8aa7ebeecaaaa66cdba37ba017
bchavdarov <spo@abv.bg>
2020-08-04 17:56:42 +0300

    updated file

--- bcdl-block-gallery.php
+++ bcdl-block-gallery.php
@@ -7,7 +7,7 @@
  * Requires at least:  5.2
  * Requires PHP:       7.2
  * Author:             Boncho Chavdarov
- * Author URI:        https://bchavdarov.github.io/bcdlab
+ * Author URI:         https://bchavdarov.github.io/bcdlab
  * License:            GPL-2.0-or-later
  * License URI:        https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:        bcdl-block-gallery
